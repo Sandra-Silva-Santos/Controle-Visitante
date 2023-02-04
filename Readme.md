f7030b8b233ccd7146258ba824dea49b98245156
Sandra <99267898+Sandra-Silva-Santos@users.noreply.github.com>
2023-02-04 20:32:50 -0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Controle Visitante
+
+
+O repositório controle visitante é destinado a armazenar o código referente ao projeto desenvolvido durante o curso <br> <a href="https://www.udemy.com/course/djangoframeworknapratica/" target="_blank">Django framework na prática</a> da Udemy.
+
+
+
+Além de conhecer as funcionalidades do framework Django, no curso foi desenvolvido um projeto real, uma dashboard para registro e administração de visitantes de um determinado condomínio.<br><br>
+
+
+## Instalação
+
+Para instalar o projeto, é necessário ter o Python 3.11 instalado em sua máquina. Após isso, basta clonar o repositório e instalar as dependências do projeto.<br><br>
+
+## Dependências
+pip install virtualenv  
+pip install Django
---
+++ controle_visitante/urls.py
@@ -0,0 +1,66 @@
+"""controle_visitante URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
+import dashboard.views
+import visitantes.views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html"
+        ),
+        name="login"
+    ),
+
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(
+            template_name="logout.html"
+        ),
+        name="logout"
+    ),
+
+    path(
+        "",
+        dashboard.views.index,
+        name="index",
+    ),
+
+    path(
+        "registrar-vistante/",
+        visitantes.views.registrar_visitante,
+        name="registrar_visitante",
+    ),
+
+    path(
+        "visitantes/<int:id>/",
+        visitantes.views.informacoes_visitante,
+        name="informacoes_visitante",
+    ),
+
+    path(
+        "visitantes/<int:id>/finalizar-visita/",
+        visitantes.views.finalizar_visita,
+        name="finalizar_visita"
+    )
+]
